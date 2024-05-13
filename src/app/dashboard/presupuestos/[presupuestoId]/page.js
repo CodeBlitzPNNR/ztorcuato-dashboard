87566c4e5663d6e1444b0fd2eb9f3597109b7afb
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-05-13 16:48:21 -0300

    que estoy haciendo?

--- src/app/dashboard/presupuestos/[presupuestoId]/page.js
+++ src/app/dashboard/presupuestos/[presupuestoId]/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
 
 import Presupuesto from "@/app/components/Presupuesto";
@@ -7,30 +8,46 @@ export default function presupuestoInfo({ params }) {
 
   const showInfo = useAuth();
 
+  const convertToPdf = () => {
+    const html2pdf = require('html2pdf.js');
+
+    const content = document.getElementById('imprimir');
+
+    const opt = {
+      margin: [0, 0],
+      filename: `Presupuesto.pdf`,
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'em', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(content).save();
+  };
+
   return (
     <div>
-      { showInfo ? 
-      (<section className="p-3">
-        <h1 className="font-bold text-xl">PRESUPUESTO N° {params.presupuestoId}</h1>        
-          <Presupuesto params={ params.presupuestoId } />
-        <div className="flex justify-center mt-4">
-          <button  className="px-4 py-2 bg-emerald-700 text-white font-bold rounded-lg hover:bg-emerald-400">CREAR PDF</button>
-        </div>
-      </section>
-      ) : (
-        <div className="w-full h-full flex justify-center items-center">
-          <div className="bg-slate-300 p-11 rounded-xl items-center flex flex-col gap-4">
-            <h6>Las credenciales no son válidas o caducaron.</h6>
-            <a
-              href="/"
-              className="bg-slate-700 text-white py-2 px-4 rounded-xl"
-            >
-              Volver al login
-            </a>
+      {showInfo ?
+        (<section className="p-3">
+          <h1 className="font-bold text-xl">PRESUPUESTO N° {params.presupuestoId}</h1>
+          <Presupuesto params={params.presupuestoId} />
+          <div className="flex justify-center mt-4">
+            <button onClick={convertToPdf} className="px-4 py-2 bg-emerald-700 text-white font-bold rounded-lg hover:bg-emerald-400">CREAR PDF</button>
           </div>
-        </div>
-      )
-    }
+        </section>
+        ) : (
+          <div className="w-full h-full flex justify-center items-center">
+            <div className="bg-slate-300 p-11 rounded-xl items-center flex flex-col gap-4">
+              <h6>Las credenciales no son válidas o caducaron.</h6>
+              <a
+                href="/"
+                className="bg-slate-700 text-white py-2 px-4 rounded-xl"
+              >
+                Volver al login
+              </a>
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 }
